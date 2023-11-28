a0e0abc6975638e1a259b7c60ea5c8453f0002d5
Sandra <sluukas@gmail.com>
2023-11-28 11:53:42 +0100

    added devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_165528) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_103915) do
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
@@ -70,9 +70,14 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_165528) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "follows", "users", column: "followee_id"
