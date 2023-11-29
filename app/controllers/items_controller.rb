92b18c742e67bd9375ec3cf3e53ebda5c319dd93
Camilla Ligovic <camilla.ligovic@gmail.com>
2023-11-29 16:45:50 +0100

    Created items  index view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
 
-
-
+  def index
+    @items = Item.where(swappable: true)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root "pages#landing"
+
+  get '/home', to: 'pages#home'
+
+  resources :items, only: [:index]
+
 end
