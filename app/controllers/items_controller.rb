00451100616baf6175a858907a416b23b33c2282
Sandra <sluukas@gmail.com>
2023-11-29 12:46:55 +0100

    new item form implemented

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,23 @@
 class ItemsController < ApplicationController
 
+  def new
+    @item = Item.new
+  end
 
+  def create
+    @item = Item.new(items_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new, status: 422
+    end
 
+  end
+
+  private
+
+  def items_params
+    params.require(:item).permit(:title, :description, :swappable,:category_id, :photo)
+  end
 end
