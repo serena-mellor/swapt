6920b8fff0bde81a71a2ca57ecf7d18fa2e19e8b
Serena Mellor <serenamellor@outlook.com>
2023-11-30 15:38:48 +0100

    Show Pages with if/else

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,22 @@
 class ItemsController < ApplicationController
 skip_before_action :authenticate_user!, only: [:index, :show]
-  
+
   def show
     @item = Item.find(params[:id])
   end
 
   def index
     @items = Item.where(swappable: true)
-    
+  end
+
   def new
     @item = Item.new
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
   end
 
   def create
@@ -20,9 +27,13 @@ skip_before_action :authenticate_user!, only: [:index, :show]
     else
       render :new, status: 422
     end
-
   end
 
+  def update
+    @item = Item.find(params[:id])
+    @item.update(items_params)
+    redirect_to items_path
+  end
   private
 
   def items_params
