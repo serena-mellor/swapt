a700e5b8cfa5162594eebac610585d61a245a0af
Camilla Ligovic <camilla.ligovic@gmail.com>
2023-12-06 09:53:13 +0100

    saved changes

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -3,7 +3,11 @@ class Outfit < ApplicationRecord
   has_many :outfit_items, dependent: :destroy
   has_many :items, through: :outfit_items
 
+  # def sort_items_by_position
+  #   items.sort_by { |item| item.category.position }
+  # end
+
   def sort_items_by_position
-    items.sort_by { |item| item.category.position }.reverse
+    items.includes?(:category).sort_by { |item| Category.positions[item.category.position] }
   end
 end
