694caeb24bb25f154904c477e80aca8376565675
Serena Mellor <serenamellor@outlook.com>
2023-12-08 11:17:48 +0100

    Fixed Glitches on Map

--- app/javascript/controllers/map_toggle_controller.js
+++ app/javascript/controllers/map_toggle_controller.js
@@ -3,35 +3,39 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="map-toggle"
 export default class extends Controller {
 
-  static targets = ["mapToggle", "switch"];
+  static targets = ["mapToggle", "switch", "sliderShowMapInput"];
 
   connect() {
     console.log("Hello from toggle_controller.js")
     console.log(typeof sessionStorage.getItem("showMap"));
-    if(sessionStorage.getItem("showMap") == "true") {
-      this.mapToggleTarget.classList.remove("d-none")
-      this.switchTarget.setAttribute("checked", true)
-    }
+    // if(sessionStorage.getItem("showMap") == "true") {
+    //   this.mapToggleTarget.classList.remove("d-none")
+    //   this.switchTarget.setAttribute("checked", true)
+    // }
   }
 
   map() {
     this.mapToggleTarget.classList.toggle("d-none");
 
-    if(sessionStorage.getItem("showMap") == "true") {
-      sessionStorage.removeItem("showMap")
+    const urlParams = (new URL(window.location.href)).searchParams;
+
+    if (urlParams.get("map") === "1") {
+      urlParams.set("map", "0")
+      this.sliderShowMapInputTarget.value = "0"
     } else {
-      sessionStorage.setItem("showMap", true)
-    }
+      urlParams.set("map", "1")
+      this.sliderShowMapInputTarget.value = "1"
     }
 
-    // const urlParams = (new URL(window.location.href)).searchParams;
-
-    // if (urlParams.get("map") === "1") {
-    //   urlParams.set("map", "0")
+    history.replaceState(null, "", `${window.location.pathname}?${urlParams}`)
+    // if(sessionStorage.getItem("showMap") == "true") {
+    //   sessionStorage.removeItem("showMap")
     // } else {
-    //   urlParams.set("map", "1")
+    //   sessionStorage.setItem("showMap", true)
     // }
+    }
+
+
 
     // console.log(urlParams.toString())
-    // history.replaceState(null, "", `${window.location.pathname}?${urlParams}`)
 }
